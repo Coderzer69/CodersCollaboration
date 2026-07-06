6f75d8b0b09c98f2d46483147c55b424b3a8ba7c
Urvish Bhardwaj <urvishbhardwaj7@gmail.com>
2026-07-06 11:15:21 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,111 @@
+// //  import express from "express";
+// // import { createServer} from "http";
+// // import { Socket } from "socket.io";
+// // import {YSocketIO} from "y-socket.io/dist/server"
+
+
+
+// // const app = express();
+// // const httpServer =  createServer(app);
+
+// // const io = new Server(httpServer,{
+// //     cors:{
+// //         origin:"*",
+// //         methods : ["GET","POST"]
+// //     }
+// // })
+// // const ySocketIO = new  YSocketIO(io)
+// // ySocketIO.initialize()
+
+// // // app.get("/",(req,res)=>{
+// // //     res.status(200).json({
+// // //         message:"Wass Up Dwag",
+// // //         success:"true"
+// // //     })
+// // // })
+
+
+// // app.get('/healthCheck' ,(req,res)=>{
+// //     res.status(200).json({
+// //         message:"ok",
+// //         success:true
+// //     })
+// // })
+
+// // httpServer.listen(3000 , () =>{
+// //     console.log("Server is running on Port 3000")
+// // })
+
+ 
+
+// import express from "express"
+// import { createServer } from "http"
+// import { Server } from "socket.io"
+// import { YSocketIO } from "y-socket.io/dist/server"
+
+
+// const app = express()
+// app.use(express.static("public"))
+
+
+// const httpServer = createServer(app)
+
+// const io = new Server(httpServer, {
+//     cors: {
+//         origin: "*",
+//         methods: [ "GET", "POST" ]
+//     }
+// })
+
+
+// const ySocketIO = new YSocketIO(io)
+// ySocketIO.initialize()
+
+
+// app.get('/health', (req, res) => {
+//     res.status(200).json({
+//         message: "ok",
+//         success: true
+//     })
+// })
+
+
+// httpServer.listen(3000, () => {
+//     console.log("Server is running on port 3000")
+// })
+
+import express from "express"
+import { createServer } from "http"
+import { Server } from "socket.io"
+import { YSocketIO } from "y-socket.io/dist/server"
+
+
+const app = express()
+app.use(express.static("public"))
+
+
+const httpServer = createServer(app)
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+        methods: [ "GET", "POST" ]
+    }
+})
+
+
+const ySocketIO = new YSocketIO(io)
+ySocketIO.initialize()
+
+
+app.get('/health', (req, res) => {
+    res.status(200).json({
+        message: "ok",
+        success: true
+    })
+})
+
+
+httpServer.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
